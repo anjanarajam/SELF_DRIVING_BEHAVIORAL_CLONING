20c0086896ccf7dd69655ac967db8518e16a03a0
Anjana Rajam <anjanarsid@gmail.com>
2020-06-13 20:56:19 +0200

    Updated files, final videp and updated writeup

--- model.py
+++ model.py
@@ -17,16 +17,19 @@ from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
 from sklearn.model_selection import train_test_split
+from keras.regularizers import l2
 
 # Read image paths from csv file
 def read_csv():
     # Array to store training data
     training_data = []
     
-    with open("./data_new_track1/driving_log.csv") as csvfile:
+    with open("./data/driving_log.csv") as csvfile:
         read_file = csv.reader(csvfile)
         for line in read_file:
-            training_data.append(line)   
+            training_data.append(line)  
+            
+    print(len(training_data))
             
     training_data = training_data[1:]
 
@@ -54,11 +57,11 @@ def read_batch_data(batch_data):
             # Get the last row of the line which is the path of the image
             file_name = source_path.split('/')[-1]
             # Get the current path of the image
-            current_path = "./data_new_track1/IMG/" + file_name 
+            current_path = "./data/IMG/" + file_name 
             # read the image from the path
             image = cv2.imread(current_path)
             # Get the image appended
-            images.append(image)
+            images.append(image)            
 
         # Now that we have centre, left and right images, we need to add a correction
         # When car turns left, it is steering angle plus correction, and when the car turns right
@@ -72,7 +75,10 @@ def read_batch_data(batch_data):
         steering_angle.append(angle + correction)
         # Append the steering measurement in the array for the right image
         steering_angle.append(angle - correction)
-            
+    
+#     print("images", len(images))
+#     print("steering angke", len(steering_angle))
+    
     return images, steering_angle
 
 def flip_images(images, steering_angle):
@@ -92,6 +98,8 @@ def flip_images(images, steering_angle):
         # Append the augmented steering angle
         augmented_steering_angle.append(flipped_steering_angle)  
     
+#     print("aug_img", len(augmented_images))
+#     print("aug_ang", len(augmented_steering_angle))
     return augmented_images, augmented_steering_angle
 
 # Generate training or validation data with help of python generator
@@ -123,9 +131,12 @@ def data_generator(data_samples, batch_size=32):
             total_images.extend(images)  
             total_images.extend(augmented_images) 
             total_angles.extend(steering_angle)  
-            total_angles.extend(augmented_steering_angle)       
+            total_angles.extend(augmented_steering_angle)    
             
-            # Get the X and Y training set: the total_images are the features per batch
+#             print("tot_img", len(total_images))
+#             print("tot_ang", len(total_angles))
+            
+            # Get the X and Y training set: the total_images are features per batch
             # and the total_angles is the label per batch
             X_train_batch, Y_train_batch = np.asarray(total_images), np.asarray(total_angles)
 
@@ -147,14 +158,17 @@ def preprocess_data_layers():
 # Create nvdia model
 def nvdia_cnn(model):
     # Add a convolution layers - 6 filters, 5x5 each
-    model.add(Conv2D(24, (5,5), subsample=(2,2), activation='relu'))
-    model.add(Conv2D(36, (5,5), subsample=(2,2), activation='relu'))
-    model.add(Conv2D(48, (5,5), subsample=(2,2), activation='relu'))
-    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(Conv2D(24, (5,5), subsample=(2,2), activation='relu', W_regularizer=l2(0.001)))
+    model.add(Conv2D(36, (5,5), subsample=(2,2), activation='relu', W_regularizer=l2(0.001)))
+    model.add(Conv2D(48, (5,5), subsample=(2,2), activation='relu', W_regularizer=l2(0.001)))
+    model.add(Conv2D(64, (3,3), activation='relu', W_regularizer=l2(0.001)))
     model.add(Flatten())
-    model.add(Dense(100))
-    model.add(Dense(50))
-    model.add(Dense(10))
+    model.add(Dense(100, W_regularizer=l2(0.001)))
+    model.add(Dropout(0.25))
+    model.add(Dense(50, W_regularizer=l2(0.001)))
+    model.add(Dropout(0.25))
+    model.add(Dense(10, W_regularizer=l2(0.001)))
+    model.add(Dropout(0.25))
     # Final is a single node
     model.add(Dense(1))
     
@@ -179,7 +193,8 @@ model = preprocess_data_layers()
 # Create nvdia model
 model = nvdia_cnn(model)
 # Compile the model
-model.compile(loss='mse', optimizer='adam')
+#model.compile(loss='mse', optimizer='adam')
+model.compile(optimizer=Adam(lr=1e-4), loss='mse')
 # Model summary
 model.summary()
 # Train the model
@@ -199,6 +214,7 @@ plt.title('model mean squared error loss')
 plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('loss.png')
 # plt.show()
     
     
